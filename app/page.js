897290a55abd991fc2edc4d8906eb85901b9ca5f
Jopzik <jualexmora@gmail.com>
2024-10-17 13:59:32 -0700

    Add icons

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import s from './delete.module.css'
+import { BoxAdd } from 'iconsax-react'
 
 export default function Home() {
   return (
     <div className={s.container}>
+      <BoxAdd
+        size="128"
+        color="#0573E6"
+        variant="Bulk"
+      />
       <h1>Next Boilerplate + Design Tokens</h1>
       <p>By <a
         target="_blank"
